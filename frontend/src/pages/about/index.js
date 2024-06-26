16c27478bc572ddf4301d37aab1300d050db4f7c
LenarSag <saglenar@gmail.com>
2024-06-26 20:36:25 +0300

    edited about

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/LenarSag/foodgram" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://t.me/Lenar_ST" className={styles.textLink}>Ленар Сагындыков</a>
             </p>
           </div>
         </aside>
