16466f0da64de4bb5143daaefcd0a10db1fedb25
LenarSag <saglenar@gmail.com>
2024-06-26 20:00:45 +0300

    updated redirect for short url

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponseNotFound
-from django.shortcuts import redirect
-from django.urls import reverse
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 
 from .utils import get_decoded_short_url
 
@@ -11,10 +9,9 @@ def handle_short_url(request, short_url):
         decoded_values = get_decoded_short_url(short_url)
         if decoded_values:
             recipe_id = decoded_values[0]
-            recipe_detail_url = reverse(
-                "api:recipes-detail", args=(recipe_id,)
+            return HttpResponseRedirect(
+                request.build_absolute_uri(f"/recipes/{recipe_id}/")
             )
-            return redirect(recipe_detail_url)
         return HttpResponseNotFound(
             "URL-адрес недействителен или срок его действия истек."
         )
